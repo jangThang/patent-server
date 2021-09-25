84cfc9f7655ea519f31a887b6e1ba7e08c6f3ded
jangThang <48477448+jangThang@users.noreply.github.com>
2021-09-26 03:42:44 +0900

    전체 페이지 부트스트랩 적용

--- patent/urls.py
+++ patent/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('', views.PostList.as_view())
+    path('', views.PostList.as_view()),
+
+    path('descript/', views.descript),
+    path('word2vec/', views.word2vec),
+    path('lda/', views.lda),
+    path('lsa/', views.lsa)
 ]
--- patent/views.py
+++ patent/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from .models import Post
 
 class PostList(ListView):
@@ -8,3 +9,27 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 # Create your views here.
+
+def descript(request):
+    return render(
+        request,
+        'patent/descript.html'
+    )
+
+def word2vec(request):
+    return render(
+        request,
+        'patent/word2vec.html'
+    )
+
+def lda(request):
+    return render(
+        request,
+        'patent/LDA.html'
+    )
+
+def lsa(request):
+    return render(
+        request,
+        'patent/LSA.html'
+    )
