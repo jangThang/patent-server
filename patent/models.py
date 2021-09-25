9ddd8d91853ef91011d45eee0945d5fb7790cbde
jangThang <48477448+jangThang@users.noreply.github.com>
2021-09-26 01:58:55 +0900

    부트스트랩 적용

--- patent/models.py
+++ patent/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/patent/{self.pk}'
 # Create your models here.
--- patent/views.py
+++ patent/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 # Create your views here.
